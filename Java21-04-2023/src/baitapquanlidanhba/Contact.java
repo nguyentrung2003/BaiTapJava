96ca48d1beb57ea1cc8ce947c8ff145c779ba419
Nguyen Trung <nguyentrung.humgg@gmail.com>
2023-04-21 23:32:23 +0700

    add . bai tap 21 - 04 - 2023

---
+++ Java21-04-2023/src/baitapquanlidanhba/Contact.java
@@ -0,0 +1,7 @@
+package baitapquanlidanhba;
+public abstract class Contact {
+    abstract void insertContact(String name, String phone_number);
+    abstract void deleteContact(String name);
+    abstract void updateContact(String name, String new_number);
+    abstract void searchContact(String name);
+}
---
+++ Java21-04-2023/src/hinhhoc/HinhVuong.java
@@ -0,0 +1,8 @@
+package hinhhoc;
+
+
+public class HinhVuong extends HinhChuNhat {
+	public HinhVuong(String Ten,float Canh) {
+		super(Ten, Canh, Canh);
+	}
+}
